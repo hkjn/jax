8bcbc3fca5317f0c74d667fe2a33fb97c017d6dd
Henrik Jonsson <me@hkjn.me>
2016-11-05 15:23:04 +0100

    Rename to square() and call twice

--- sub/sub.go
+++ sub/sub.go
@@ -1,17 +1,28 @@
 // This is Aruna learning how to code in go
 package main
 
-//Allows me to use the fmt pakage which can format things and print them to the terminal
+// Allows me to use the fmt pakage which can format things and print them to the terminal
 import "fmt"
 
+// main.main() ->
+//   fmt.Println()
+//   f()
+//   fmt.Printf()
+//   f()
+//   fmt.Println()
+//   fmt.Printf()
 func main() {
-	//We will run this function when we start the program
+	// We will run this function when we start the program
 	fmt.Println("oivey")
-	q := f(8)
+	q := square(8)
 	fmt.Printf("fucking q is %d!?!?!?\n", q)
+	z := square(100)
+	k := square(z)
+	fmt.Println("oibayayushki!!")
+	fmt.Printf("k is %d!\n", k)
 }
 
-func f(x int) int {
+func square(x int) int {
 	y := x * x
 	return y
 }
