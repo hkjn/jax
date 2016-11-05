3b46376d8e88392aa2d9e9c36a6717b738867af0
Henrik Jonsson <me@hkjn.me>
2016-11-05 13:53:12 +0100

    Add sub.go, Aruna's first go program

---
+++ sub/sub.go
@@ -0,0 +1,18 @@
+//This is Aruna learning how to code in go
+
+package main
+
+//Allows me to use the fmt pakage which can format things and print them to the terminal
+import "fmt"
+
+func main() {
+	//We will run this function when we start the program
+	fmt.Println("oivey")
+	q := f(8)
+	fmt.Printf("fucking q is %d!?!?!?\n", q)
+}
+
+func f(x int) int {
+	y := x * x
+	return y
+}
